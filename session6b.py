506f171419a0f6100c10392d8846bb5de8eb48a6
ajitg9572 <59866787+ajitg9572@users.noreply.github.com>
2020-01-21 19:30:36 +0530

    Add files via upload

---
+++ session6b.py
@@ -0,0 +1,12 @@
+def square():
+    num = 20
+    print(">> num in square is:", num)
+
+def main():
+    num = 10
+    print(">> num in main is:", num)
+    square()
+    print(">> This is main's Last Statement")
+
+if __name__ == "__main__":
+    main()
